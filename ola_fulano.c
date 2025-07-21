b13065c07ae81764fe6fab2cfc18dbbcc55dfa14
Tainá Meirelles <tainaramosmeirelles@gmail.com>
2025-07-21 14:51:25 -0300

    Update ola_fulano.c

--- ola_fulano.c
+++ ola_fulano.c
@@ -1,22 +1,3 @@
-/* IDENTIFICAÇÃO DO ESTUDANTE:
- * Preencha seus dados e leia a declaração de honestidade abaixo. NÃO APAGUE
- * nenhuma linha deste comentário de seu código!
- *
- *    Nome completo:Tainá Ramos de Jesus Meirelles
- *    Matrícula:202527909
- *    Turma:CC1Nb
- *    Email:tainaramosmeirelles@gmail.com
- *
- * DECLARAÇÃO DE HONESTIDADE ACADÊMICA:
- * Eu afirmo que o código abaixo foi de minha autoria. Também afirmo que não
- * pratiquei nenhuma forma de "cola" ou "plágio" na elaboração do programa,
- * e que não violei nenhuma das normas de integridade acadêmica da disciplina.
- * Estou ciente de que todo código enviado será verificado automaticamente
- * contra plágio e que caso eu tenha praticado qualquer atividade proibida
- * conforme as normas da disciplina, estou sujeito à penalidades conforme
- * definidas pelo professor da disciplina e/ou instituição.
- */
-
 #include <cs50.h>
 #include <stdio.h>
 int main(void)
